fe34d6a4f48a54c6651ec1273bebcd7fe77795e4
Anton <anton.arboleda2gmail.com>
2016-10-06 17:17:04 -0700

    added up/down/left/right/ functionality to keys of game and refactored the code

--- js/app.js
+++ js/app.js
@@ -1,55 +1,41 @@
-// Enemies our player must avoid
+// Enemies the player must avoid
 var Enemy = function( x, y ) {
     // Variables applied to each of our instances go here,
     //x and y coordinates are the coordinates on the board
+
     var canvasWidth = 202, canvasHeight = 295
     this.x = x
-    this.y = y//canvasHeight / 3 + Math.random() * canvasHeight / 2
+    this.y = y
     this.xVelocity = 1;
     this.yVelocity = 0;
     this.active = true;
      
-
-    // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+//This updates the players from left to right
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    // this.x += ;
-    this.x += this.xVelocity * dt / 4;
 
-    this.y += this.yVelocity;
-    
+    this.x += this.xVelocity * dt / 4;  
      
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function(x , y ) {
-    // ctx.drawImage(Resources.get(this.sprite), 11, 62);
-  
-   //this is to draw the image. x and y will be constantly changing
+// This method draws the enemy on the screen
+Enemy.prototype.render = function( x , y ) {
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    // a handleInput() method.
-    // ctx.drawImage(Resources.get(this.sprite), 11, 228);
 
 };
-
 
 
 // Now write your own player class
 var Player = function (x, y) {
-    this.x = x;
-    this.y = y;
+    this.x = x || 200;
+    this.y = y || 400;
     this.sprite = 'images/char-boy.png'
-
 }
     
 // This class requires an update(), render() and
@@ -59,8 +45,7 @@ Player.prototype.update = function () {
 };  
 
 Player.prototype.render = function ( x, y ) {
-    this.x = x || 200;
-    this.y = y || 400;
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
 
 
@@ -68,34 +53,25 @@ Player.prototype.render = function ( x, y ) {
 // a handleInput() method.
 Player.prototype.handleInput = function (position) {
     if(position === 'right'){
-        player.x += 5;
-        console.log('right')
+        player.x += 100;
+        
     } else if (position === 'left'){        
-        player.x -= 5;
-        console.log('left')
+        player.x -= 100;   
     } else if (position === 'up') {
-        player.y += 5;
-        console.log('up')
+        player.y -= 83;   
     } else if (position === 'down') {
-        player.y -= 5;
-        console.log('down')
+        player.y += 83;   
+
     }
 
 };
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-//Here, I created 3 enemy objects that will move across the screen
-// var allEnemies = [
-// new Enemy(0,63),
-// new Enemy(0,143),
-// new Enemy(0,230)
-// ];
+
 var allEnemies = [];
-
-window.setInterval(createEnemies, 900)
-
+//This calls the createEnemies function every 900 milliseconds to constantly generate new enemies
+window.setInterval(createEnemies, 1000)
+//this function createsEnemies using JS builtin JS math.random to put it in any given column
 function createEnemies(){
 var enemyDecider = Math.random();
 var loc;
